1b211ac616b3ffe771d8220c32f7b0e558bf7027
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-06-05 01:49:25 +0800

    feat: format, add removeTodo

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Todos from "./components/Todos2";
 import { useCallback, useState } from "react";
+import Todos from "./components/Todos3";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +8,13 @@ function App() {
   const increment = () => {
     setCount((c) => c + 1);
   };
+
+  const removeTodo = useCallback(
+    (index: number) => {
+      setTodos((t) => t.filter((_, i) => i !== index));
+    },
+    [setTodos]
+  );
 
   const addTodo = useCallback(() => {
     setTodos((t) => [...t, "new todo"]);
@@ -18,7 +25,7 @@ function App() {
       <header>header</header>
       <main>
         <div>
-          <Todos todos={todos} addTodo={addTodo} />
+          <Todos todos={todos} addTodo={addTodo} removeTodo={removeTodo} />
           Count: {count}
           <button onClick={increment}>+</button>
         </div>
