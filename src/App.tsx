0fcfe1f10be5f11829041b40ee9958b1be96910b
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-06-05 01:08:03 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import Todos from './components/Todos2';
-import { useCallback, useState } from 'react';
+import Todos from "./components/Todos2";
+import { useCallback, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState<string[]>([]);
 
   const increment = () => {
-    setCount(c => c + 1);
-  }
+    setCount((c) => c + 1);
+  };
 
   const addTodo = useCallback(() => {
-    setTodos(t => [...t, 'new todo']);
+    setTodos((t) => [...t, "new todo"]);
   }, []);
 
   return (
-    <div >
-      <header >
-        header
-      </header>
+    <div>
+      <header>header</header>
       <main>
         <div>
           <Todos todos={todos} addTodo={addTodo} />
