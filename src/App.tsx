fb742a136a90e1cfde4eaac459b43f7f6bea5e0a
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-10-09 14:01:47 +0800

    fix: build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import { useCallback, useState } from "react";
-import Todos from "./components/Todos3";
+import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [todos, setTodos] = useState<string[]>([]);
 
   const increment = () => {
     setCount((c) => c + 1);
   };
 
-  const removeTodo = useCallback(
-    (index: number) => {
-      setTodos((t) => t.filter((_, i) => i !== index));
-    },
-    [setTodos]
-  );
-
-  const addTodo = useCallback(() => {
-    setTodos((t) => [...t, "new todo"]);
-  }, []);
-
   return (
     <div>
-      <header>header</header>
       <main>
         <div>
-          <Todos todos={todos} addTodo={addTodo} removeTodo={removeTodo} />
           Count: {count}
           <button onClick={increment}>+</button>
         </div>
