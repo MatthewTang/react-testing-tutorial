cae5e2806d45dc9235a27903ef6922fe60489659
Tang Siu Hin <matthew.tang@connect.hku.hk>
2023-10-11 17:24:56 +0800

    feat: use ref to track render count

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
+  // const [renderCount, setRenderCount] = useState(0);
+
+  // use ref to keep render count
+  const renderCount = useRef(1);
 
   const increment = () => {
     setCount((c) => c + 1);
   };
+
+  /* // this will cause an infinite loop */
+  /* useEffect(() => { */
+  /*   setRenderCount((c) => c + 1); */
+  /* }); */
+
+  // this will not cause an infinite loop
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
 
   return (
     <div>
@@ -14,6 +28,8 @@ function App() {
           Count: {count}
           <button onClick={increment}>+</button>
         </div>
+        {/* <div>Render Count: {renderCount}</div> */}
+        <div>Render Count: {renderCount.current}</div>
       </main>
     </div>
   );
